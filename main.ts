c45baf567a491a48188e759cbee7e4fdbb596772
DanielJeffrey-2 <113033888+DanielJeffrey-2@users.noreply.github.com>
2022-09-28 09:05:36 -0400

    Added comment

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+/**
+ * Created by: Daniel Jeffrey
+ * 
+ * Created on: Sept 28, 2022
+ * 
+ * This program demonstrates modifying a simple variable and then displaying it
+ */
 let hungriness = 0
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
